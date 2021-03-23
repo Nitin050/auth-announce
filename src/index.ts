5e46e84c4e6a07d4ba974d129ffd1fe818134beb
Nitin Malhotra <nitinkumalhotra@gmailcom>
2021-03-23 13:23:28 +0530

    changed cors permission

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 app.set('trust proxy', true);
 
 var corsOptions = {
-  origin: ['http://localhost:3000', 'https://ann05.vercel.app'],
+  origin: 'https://ann05.vercel.app',
   optionsSuccessStatus: 200,
   credentials: true,
   exposedHeaders: ["set-cookie"],

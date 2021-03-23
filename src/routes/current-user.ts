4f66488f30294e56013d1e05a166a996abeeaed6
Nitin Malhotra <nitinkumalhotra@gmailcom>
2021-03-23 16:28:41 +0530

    Updates

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -4,8 +4,7 @@ import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: {email: 'aa'} });
-  // res.send({ currentUser: req.currentUser || null });
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };

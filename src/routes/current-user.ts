92b5f9a6eca0f4070ae1c5990071bcc1c14c230d
Nitin Malhotra <nitinkumalhotra@gmailcom>
2021-03-23 14:23:26 +0530

    changed cors permission

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: 'yoyo' });
+  res.send({ currentUser: {email: 'aa'} });
   // res.send({ currentUser: req.currentUser || null });
 });
 
